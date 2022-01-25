6391a5b3a3df75f518743319ec31801c238cb930
OluwatofunmiAjoke <tofunmiajoke@gmail.com>
2022-01-25 16:13:21 +0100

    script for countdown is over

--- app.js
+++ app.js
@@ -4,26 +4,40 @@ const daysElement  = document.querySelector('#days');
 const hourElement  = document.querySelector('#hours');
 const minsElement  = document.querySelector('#mins');
 const secsElement  = document.querySelector('#seconds');
+const mainText = document.querySelector('h1')
 
 
 function countdownTimer(){
     const currDate = new Date();
 
     const endDate = new Date(userDate);
+
+    if (currDate === endDate | currDate > endDate){
+        mainText.innerText = "WELCOME TO 22, AJOKE!!!";
+        mainText.style.color= '#34c9eb';
+        mainText.style.fontFamily = 'Dancing Script';
+        mainText.style.fontSize = '7rem';
+        document.body.style.backgroundImage = "url('./images/over-bg.jpg')";
+        daysElement.innerHTML = '0';
+        hourElement.innerHTML = '0';
+        minsElement.innerHTML = '0';
+        secsElement.innerHTML = '0';
+    }
+    else{
+        const diff = endDate - currDate;
+
+        const totalSeconds = diff / 1000;
+        const days = Math.floor(totalSeconds/86400);
+        const hours = Math.floor(totalSeconds/3600) % 24;
+        const minutes = Math.floor(totalSeconds/60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+
+        daysElement.innerHTML = days;
+        hourElement.innerHTML = hours;
+        minsElement.innerHTML = minutes;
+        secsElement.innerHTML = seconds;
+    }
     
-    const diff = endDate - currDate;
-
-    const totalSeconds = diff / 1000;
-    const days = Math.floor(totalSeconds/86400);
-    const hours = Math.floor(totalSeconds/3600) % 24;
-    const minutes = Math.floor(totalSeconds/60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    daysElement.innerHTML = days;
-    hourElement.innerHTML = hours;
-    minsElement.innerHTML = minutes;
-    secsElement.innerHTML = seconds;
-
 }
 
 countdownTimer();
